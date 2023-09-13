1d7617a05e39a3bd6b7d13b0d17766689045d95f
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-14 01:57:27 +0800

    Added authentication provider Bean

--- src/main/java/com/kush/banbah/soloprojectbackend/config/ApplicationConfig.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/config/ApplicationConfig.java
@@ -1,12 +1,17 @@
 package com.kush.banbah.soloprojectbackend.config;
 
 import com.kush.banbah.soloprojectbackend.User.UserRepo;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,5 +25,20 @@ public class ApplicationConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
