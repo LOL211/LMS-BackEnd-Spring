05ad26bd5467e5d8da1ab65227fab44eeb33ceaf
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-14 01:58:24 +0800

    Created Security Config class

---
+++ src/main/java/com/kush/banbah/soloprojectbackend/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.kush.banbah.soloprojectbackend.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        try {
+            http
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                        // specify your request matcher here
+                        .requestMatchers("")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                    .sessionManagement(sess -> sess
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authenticationProvider( authenticationProvider)
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
