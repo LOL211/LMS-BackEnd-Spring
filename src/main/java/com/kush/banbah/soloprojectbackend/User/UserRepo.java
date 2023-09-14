4662ec50a6ffc263971fa97e620792c3841bad37
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-14 12:51:04 +0800

    Fixed typo

--- src/main/java/com/kush/banbah/soloprojectbackend/User/UserRepo.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/User/UserRepo.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface UserRepo extends JpaRepository<User, Integer> {
-    Optional<User> findbyEmail(String email);
+    Optional<User> findByEmail(String email);
 }
