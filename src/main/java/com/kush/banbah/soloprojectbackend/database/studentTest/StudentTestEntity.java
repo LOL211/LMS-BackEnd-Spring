21fb2926b2f088505e79ff453d82537d6653dc59
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-14 21:04:30 +0800

    StudentTestEntity and repo creation

---
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/studentTest/StudentTestEntity.java
@@ -0,0 +1,38 @@
+package com.kush.banbah.soloprojectbackend.database.studentTest;
+
+
+import com.kush.banbah.soloprojectbackend.database.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_test")
+@IdClass(UserTestId.class)
+public class StudentTestEntity {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="test_id")
+    private TestsEntity test;
+    @Id
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private UserEntity user;
+    private int score;
+}
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class UserTestId implements Serializable {
+    private UserEntity user;
+    private TestsEntity test;
+}
