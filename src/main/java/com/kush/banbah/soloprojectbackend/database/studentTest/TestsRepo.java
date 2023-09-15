7080102688bb67427ee86318e1ad1be40cb84fa8
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-15 11:02:41 +0800

    Added new query to find all tests given classlist

--- src/main/java/com/kush/banbah/soloprojectbackend/database/studentTest/TestsRepo.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/studentTest/TestsRepo.java
@@ -4,10 +4,12 @@ package com.kush.banbah.soloprojectbackend.database.studentTest;
 import com.kush.banbah.soloprojectbackend.database.classes.ClassEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface TestsRepo extends JpaRepository<TestsEntity, Integer> {
-    Optional<Set<TestsEntity>> findAllByClassName(ClassEntity classEntity);
+    Optional<List<TestsEntity>> findByClassName(ClassEntity classEntity);
+
+    Optional<List<TestsEntity>> findAllByClassNameIn(List<ClassEntity> classList);
 
 }
