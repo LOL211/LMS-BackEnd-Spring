465c5820e92a6e96296e38085f13fd12404a4abe
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-15 11:01:38 +0800

    Updated return type to list

--- src/main/java/com/kush/banbah/soloprojectbackend/database/studentTest/StudentTestsRepo.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/studentTest/StudentTestsRepo.java
@@ -3,13 +3,15 @@ package com.kush.banbah.soloprojectbackend.database.studentTest;
 import com.kush.banbah.soloprojectbackend.database.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface StudentTestsRepo extends JpaRepository<StudentTestEntity, UserTestId> {
 
-    Optional<Set<StudentTestEntity>> findAllByTest(TestsEntity testsEntity);
-    Optional<Set<StudentTestEntity>> findAllByUser(UserEntity testsEntity);
+    Optional<List<StudentTestEntity>> findAllByTest(TestsEntity testsEntity);
+
+    Optional<List<StudentTestEntity>> findAllByUser(UserEntity testsEntity);
+
     Optional<StudentTestEntity> findByUserAndTest(UserEntity user, TestsEntity test);
 
 }
