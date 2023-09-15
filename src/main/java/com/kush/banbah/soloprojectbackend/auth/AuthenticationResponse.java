2be26c37d6c9f22213aa7c27e0bf28d486fffd11
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-15 12:18:44 +0800

    removed extra variable

--- src/main/java/com/kush/banbah/soloprojectbackend/auth/AuthenticationResponse.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/auth/AuthenticationResponse.java
@@ -3,12 +3,10 @@ package com.kush.banbah.soloprojectbackend.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class AuthenticationResponse {
 
     private String token;
