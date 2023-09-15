66039d18bb1fb28398335585d6a646412797ede9
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-15 11:00:40 +0800

    Added new query to get all classnames for a list of strings

--- src/main/java/com/kush/banbah/soloprojectbackend/database/classes/ClassRepo.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/classes/ClassRepo.java
@@ -2,10 +2,14 @@ package com.kush.banbah.soloprojectbackend.database.classes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClassRepo extends JpaRepository<ClassEntity, String> {
 
     Optional<ClassEntity> findByClassName(String className);
+
+    Optional<List<ClassEntity>> findByClassNameIn(List<String> classList);
+
 
 }
