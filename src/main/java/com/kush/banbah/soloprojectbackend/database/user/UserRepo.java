0b01853c5f9c8cdf1ff9f029fcfa3cbb0c9dffba
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-15 11:03:13 +0800

    Created a query to find by role Created a query to find list of classes given a user_id

--- src/main/java/com/kush/banbah/soloprojectbackend/database/user/UserRepo.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/user/UserRepo.java
@@ -1,11 +1,20 @@
 package com.kush.banbah.soloprojectbackend.database.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByEmail(String email);
+
+    Optional<List<UserEntity>> findAllByRole(UserEntity.Role role);
+
+    @Query(value = "SELECT uc.class_name FROM user_classes uc where user_id=:user_id", nativeQuery = true)
+    Optional<List<String>> findClassNamesByUser_id(@Param("user_id") int u_id);
+
 }
