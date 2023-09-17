03f908c1ad6de6ef7445891cb79bcf65076c1f89
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-17 15:07:57 +0800

    Added validation messages to auth request

--- src/main/java/com/kush/banbah/soloprojectbackend/auth/AuthenticationRequest.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/auth/AuthenticationRequest.java
@@ -2,13 +2,15 @@ package com.kush.banbah.soloprojectbackend.auth;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
 public class AuthenticationRequest {
-    @NotNull
-    @NotBlank
+    @NotNull(message = "Email is missing")
+    @NotBlank(message = "Email is blank")
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Email is not in proper format")
     private String email;
     @NotNull
     @NotBlank
