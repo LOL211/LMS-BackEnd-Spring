5caf7dba1b18542befae229a3c40709cc4391ae1
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-19 18:39:58 +0800

    Created class as response type for test request

---
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/StudentTestResponse.java
@@ -0,0 +1,10 @@
+package com.kush.banbah.soloprojectbackend.controller.testsDetails;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class StudentTestResponse {
+    private String testName;
+    private int score;
+}
