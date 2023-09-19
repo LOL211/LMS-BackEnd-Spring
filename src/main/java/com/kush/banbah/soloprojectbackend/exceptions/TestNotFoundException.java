30857121533996fbad2c8b4fdc1e20f4e0a1ed8d
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-19 18:41:10 +0800

    Missing test exception added

---
+++ src/main/java/com/kush/banbah/soloprojectbackend/exceptions/TestNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kush.banbah.soloprojectbackend.exceptions;
+
+public class TestNotFoundException extends Exception {
+    public TestNotFoundException(String message) {
+        super(message);
+    }
+}
