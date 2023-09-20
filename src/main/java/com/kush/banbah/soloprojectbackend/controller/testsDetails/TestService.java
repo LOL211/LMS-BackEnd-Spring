b1791ce9329f87d92472335d5ea929a902adf87f
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-20 12:33:51 +0800

    Updated for refactored names Updated for removal of optional Created new method for teacherTestRequest

--- src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/TestService.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/TestService.java
@@ -1,13 +1,13 @@
 package com.kush.banbah.soloprojectbackend.controller.testsDetails;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kush.banbah.soloprojectbackend.database.classes.ClassEntity;
+import com.kush.banbah.soloprojectbackend.database.classes.Class;
 import com.kush.banbah.soloprojectbackend.database.classes.ClassRepo;
-import com.kush.banbah.soloprojectbackend.database.studentTest.StudentTestEntity;
+import com.kush.banbah.soloprojectbackend.database.studentTest.StudentTest;
 import com.kush.banbah.soloprojectbackend.database.studentTest.StudentTestsRepo;
-import com.kush.banbah.soloprojectbackend.database.studentTest.TestsEntity;
+import com.kush.banbah.soloprojectbackend.database.studentTest.Tests;
 import com.kush.banbah.soloprojectbackend.database.studentTest.TestsRepo;
-import com.kush.banbah.soloprojectbackend.database.user.UserEntity;
+import com.kush.banbah.soloprojectbackend.database.user.User;
 import com.kush.banbah.soloprojectbackend.database.user.UserRepo;
 import com.kush.banbah.soloprojectbackend.exceptions.*;
 import lombok.AllArgsConstructor;
@@ -30,24 +30,23 @@ public class TestService {
     private final ClassRepo classRepo;
 
     public String getStudentScores(String className, Authentication auth) throws UserDoesNotBelongToClassException, ClassDoesNotExistException {
-        UserEntity user = (UserEntity) auth.getPrincipal();
+        User user = (User) auth.getPrincipal();
 
-        List<String> classList = userRepo.findClassNamesByUser_id(user.getId()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        List<Class> classList = classRepo.findClassByStudents(user);
 
-        ClassEntity classEntity = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
+        Class aClass = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
 
-        if (!classList.contains(className))
+        if (!classList.contains(aClass))
             throw new UserDoesNotBelongToClassException("User " + user.getName() + " does not belong to " + className);
 
 
-        List<TestsEntity> tests = testsRepo.findByClassName(classEntity).orElseThrow(NullPointerException::new);
+        List<Tests> tests = testsRepo.findByClassName(aClass);
 
-
-        List<StudentTestEntity> studentTestEntities = studentTestsRepo.findAllByUserAndTest(user, tests).orElse(null);
+        List<StudentTest> studentTestEntities = studentTestsRepo.findAllByStudentAndTest(user, tests);
         List<StudentTestResponse> responseMap = new ArrayList<>();
 
         studentTestEntities.forEach(val ->
-                responseMap.add(new StudentTestResponse(val.getTest().getTest_name(), val.getScore()))
+                responseMap.add(new StudentTestResponse(val.getTest().getTestName(), val.getScore()))
         );
         ObjectMapper mapper = new ObjectMapper();
 
@@ -56,39 +55,46 @@ public class TestService {
 
     public String getTeacherTests(String className, Authentication auth) throws ClassDoesNotExistException, NotTeacherOfClassException {
 
-        UserEntity user = (UserEntity) auth.getPrincipal();
+        User user = (User) auth.getPrincipal();
 
 
-        ClassEntity classEntity = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
+        Class aClass = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
 
-        if (classEntity.getTeacher().getId() != user.getId())
-            throw new NotTeacherOfClassException(user.getName() + " does not teach " + classEntity.getClassName());
+        if (aClass.getTeacher().getId() != user.getId())
+            throw new NotTeacherOfClassException(user.getName() + " does not teach " + aClass.getClassName());
 
-        List<TestsEntity> tests = testsRepo.findByClassName(classEntity).orElseThrow(NullPointerException::new);
+        List<Tests> tests = testsRepo.findByClassName(aClass);
 
 
         ObjectMapper mapper = new ObjectMapper();
-        List<String> testNames = tests.stream().map(TestsEntity::getTest_name).toList();
-
+        List<String> testNames = tests.stream().map(Tests::getTestName).toList();
 
         return mapper.valueToTree(testNames).toString();
     }
 
-    public String getTeacherStudentScores(String className, int test_id, Authentication auth) throws ClassDoesNotExistException, TestNotFoundException, TestDoesNotBelongToClassException, NotTeacherOfClassException {
-        UserEntity user = (UserEntity) auth.getPrincipal();
+    public String getTeacherStudentScores(String className, String testName, Authentication auth) throws ClassDoesNotExistException, TestNotFoundException, TestDoesNotBelongToClassException, NotTeacherOfClassException {
+        User user = (User) auth.getPrincipal();
 
 
-        ClassEntity classEntity = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
-        if (classEntity.getTeacher().getId() != user.getId())
-            throw new NotTeacherOfClassException(user.getName() + " does not teach " + classEntity.getClassName());
+        Class aClass = classRepo.findByClassName(className).orElseThrow(() -> new ClassDoesNotExistException(className));
+        if (aClass.getTeacher().getId() != user.getId())
+            throw new NotTeacherOfClassException(user.getName() + " does not teach " + aClass.getClassName());
 
 
-        TestsEntity test = testsRepo.findById(test_id).orElseThrow(() -> new TestNotFoundException("Test of " + test_id + " does not exist"));
+        Tests test = testsRepo.findByTestName(testName).orElseThrow(() -> new TestNotFoundException("Test of " + testName + " does not exist"));
+        System.out.println(test);
 
-        if (!test.getClassName().equals(classEntity.getClassName()))
-            throw new TestDoesNotBelongToClassException("Test " + test_id + " does not belong to class " + className);
 
-        List<UserEntity> students = userRepo.findByClasses(classEntity).orElse(null);
+        if (!test.getClassName().getClassName().equals(aClass.getClassName()))
+            throw new TestDoesNotBelongToClassException("Test " + testName + " does not belong to class " + className);
+
+//        List<UserEntity> students = userRepo.findByClassesAndRoleIs(classEntity, UserEntity.Role.STUDENT).orElseThrow(NullPointerException::new);
+//
+//        students.forEach(System.out::println);
+//
+//        ;
+//
+//        studentTestsRepo.findAllStudentTestByTest(students.stream().mapToInt(UserEntity::getId).toArray(),test.getId()).orElse(null).forEach(System.out::println);
 
         return "";
 //        List<StudentTestEntity> testsTaken = studentTestsRepo.findAllByTest(test);
