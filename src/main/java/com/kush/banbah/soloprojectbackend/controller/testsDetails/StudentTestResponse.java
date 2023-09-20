1cb151a70a08869130439b78ddef696a2d1c86b7
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-20 12:30:23 +0800

    Added all args constructor

--- src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/StudentTestResponse.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/StudentTestResponse.java
@@ -1,9 +1,10 @@
 package com.kush.banbah.soloprojectbackend.controller.testsDetails;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StudentTestResponse {
     private String testName;
     private int score;
