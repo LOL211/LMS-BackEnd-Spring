b1e4805735724ae90292479f1b2f79befb0b7f7a
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-21 12:03:22 +0800

    Class to parse request

---
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/ResponseAndRequest/ScoreUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.kush.banbah.soloprojectbackend.controller.testsDetails.ResponseAndRequest;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ScoreUpdateRequest {
+    private String newScore;
+}
