b438b4ab897688ab9fba381c598067cca1d7c0c8
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-21 16:43:56 +0800

    Updated with non-null requirement for newScore

--- src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/ResponseAndRequest/ScoreUpdateRequest.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/testsDetails/ResponseAndRequest/ScoreUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.kush.banbah.soloprojectbackend.controller.testsDetails.ResponseAndRequest;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ScoreUpdateRequest {
+    @NotNull(message = "Score cannot be empty")
     private String newScore;
 }
