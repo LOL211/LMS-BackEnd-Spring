fa36a86185f6eb0e247c7643e0a69c7967bbebe1
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-22 11:55:52 +0800

    removed - files will not be stored on DB and stored locally instead

--- src/main/java/com/kush/banbah/soloprojectbackend/database/StoredFiles/StoredFileRepo.java
+++
@@ -1,6 +0,0 @@
-package com.kush.banbah.soloprojectbackend.database.StoredFiles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoredFileRepo extends JpaRepository<StoredFile, Integer> {
-}
