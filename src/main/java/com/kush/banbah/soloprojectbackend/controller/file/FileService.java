facffa4691d73b58de2f49f9cf6dcfe834010afa
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-22 11:57:07 +0800

    changed store function to use local file system instead

--- src/main/java/com/kush/banbah/soloprojectbackend/controller/file/FileService.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/controller/file/FileService.java
@@ -2,39 +2,57 @@ package com.kush.banbah.soloprojectbackend.controller.file;
 
 
 import com.kush.banbah.soloprojectbackend.database.StoredFiles.StoredFile;
-import com.kush.banbah.soloprojectbackend.database.StoredFiles.StoredFileRepo;
+import com.kush.banbah.soloprojectbackend.exceptions.InvalidRequestException;
+import com.kush.banbah.soloprojectbackend.exceptions.InvalidRequestExceptions.FileAlreadyExistsException;
+import com.kush.banbah.soloprojectbackend.exceptions.InvalidRequestExceptions.FileIsEmptyException;
 import com.kush.banbah.soloprojectbackend.exceptions.entityNotFound.FileNotExistException;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-@AllArgsConstructor
-@Service
-public class FileService {
-    private final StoredFileRepo storedFileRepo;
 
-    public void store(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        StoredFile fileEntity = StoredFile.builder()
-                .name(fileName)
-                .type(file.getContentType())
-                .data(file.getBytes())
-                .build();
+
+@Service
+@AllArgsConstructor
+public class FileService {
+    private final Path rootLocation;
+
+    public void store(MultipartFile file) throws IOException, InvalidRequestException {
+
+
+        if (file.isEmpty())
+                throw new FileIsEmptyException("File is empty!");
+
+            Path destinationFile = this.rootLocation
+                                    .resolve(Paths.get(file.getOriginalFilename()))
+                                    .normalize().toAbsolutePath();
+
+
+            if(destinationFile.getFileName()!=null)
+                throw new FileAlreadyExistsException("File "+file.getOriginalFilename()+" already exists");
+
+            InputStream inputStream = file.getInputStream();
+            Files.copy(inputStream, destinationFile);
+            inputStream.close();
+
+
+
 
     }
 
     public StoredFile getFile(int fileID) throws FileNotExistException {
-        return storedFileRepo.findById(fileID).orElseThrow(()->new FileNotExistException("File with ID of "+fileID+"does not exist!"));
+        return null;
     }
 
     public List<StoredFile> getAllFiles() {
-        return storedFileRepo.findAll();
+        return null;
     }
 }
--- src/main/java/com/kush/banbah/soloprojectbackend/database/StoredFiles/StoredFile.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/database/StoredFiles/StoredFile.java
@@ -1,5 +1,6 @@
 package com.kush.banbah.soloprojectbackend.database.StoredFiles;
 
+import com.kush.banbah.soloprojectbackend.database.classes.Class;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,16 +11,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "files")
 public class StoredFile {
-
-    @Id
-    @GeneratedValue
-    private int file_id;
 
     private String name;
     private String type;
-//    @Lob
-    private byte[] data;
+    private Class owningClass;
+
+
+
 }
