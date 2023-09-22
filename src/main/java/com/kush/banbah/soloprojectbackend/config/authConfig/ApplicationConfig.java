8518ba1dc28c1d0b9957b50d230d5e6e9ce2c4f7
kush.banbah <kush.banbah@fdmgroup.com>
2023-09-22 11:55:35 +0800

    refactored into config package

--- src/main/java/com/kush/banbah/soloprojectbackend/config/authConfig/ApplicationConfig.java
+++ src/main/java/com/kush/banbah/soloprojectbackend/config/authConfig/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.kush.banbah.soloprojectbackend.authConfig;
+package com.kush.banbah.soloprojectbackend.config.authConfig;
 
 import com.kush.banbah.soloprojectbackend.database.user.UserRepo;
 import lombok.RequiredArgsConstructor;
